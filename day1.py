8aaca103ea54dee4054b7d34531aa35140ad70ba
syedmdthasin <162980799+syedmdthasin@users.noreply.github.com>
2024-03-13 09:18:36 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,19 @@
+
+#print("Hello world")
+#print("Hello world")
+print("Hello w@rld")
+'''
+print("Hello world")
+print("Hello world")
+'''
+    
+#(the above one #-single line commenting & ''' &"""-for multiple lines is used for commenting that
+#means the line is not come to be part of the code)
+
+
+
+import this
+print(this)
+# to import anything from outside this file or use other python files in this file we use this function
+
+import antigravity
