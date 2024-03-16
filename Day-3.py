664692ce945ccc37c42bff5e42f3fed58fc01330
syedmdthasin <162980799+syedmdthasin@users.noreply.github.com>
2024-03-16 09:13:46 +0530

    Add files via upload

---
+++ Day-3.py
@@ -0,0 +1,1080 @@
+# eg:3
+"""
+   #Take value of length and breadth of a
+   #from user and check if it is square or not.
+
+
+   #length - int(input())
+  # breadth - int(input())
+   #if length == breadth :
+  #  print("its a square")
+  # else:
+      #   print("its not square")
+
+
+  
+# Eg:4
+
+# ptthon program to check whether the
+# given integer is a multiple of both 5 and 7
+
+# n= int (input("enter the number:"))
+# if n%5==0 and %7==0:
+ #    else:
+      #   print ("no")
+
+# Eg:5
+
+ # write a progrqm to accept the cost price of a bike and display the
+# road tax to be paid
+# accoding to the following criteria :
+   #  cost price(in rs )      tax
+  #   >100000                 15% + discount 6%
+  #   >50000 and <= 100000    10%
+    # <= 5000                 5%
+
+  # price = int(input("enter the price:")
+  # amount = 0
+  #if price > = 100000:
+   discount = price*(6/100)
+  value = price - discount
+   amount = value *(15/100)
+   total = value + amount
+   else:
+       tax = price *(5/100)
+       total = price +tax
+       # print ("the onroad cost of bike is; ",total")
+       """""
+"""
+ # !-----> if elif else
+  # ! Eg:1
+ # a=7
+ # b= 9
+ # c= 30
+ # if a>b and a>c:
+  #print("A is greater*)
+ ## print("B is greater*")
+     # elif c>a and c>b;
+     # print("C is greater")
+      #a school has following rules for grading system:
+     #a. below 25 - f
+    # b. 25 to 45 -e
+    # c. 45 to 50 -d
+    # d. 50 to 55 -c
+    # e. 55 to 60 -b
+    # f. 60 above 80 -a
+
+
+  # ask user to enter marks and print the corrospondig grade
+
+   mark = int 
+
+     
+     
+ """""
+
+# ! Eg:6
+# ! accept the age of 4 people and display the oldest one ;
+
+# ! ---> short hand if else
+# Eg:1
+#a=9
+#b=60
+ #if a>b :
+     #print("a")
+     #else:
+         #print("b")
+         #----> using short hand if else
+       #   rules
+        # 1.) statement inside the if condition have to be present at first
+        # 2.) elif cannot be used in short  hand if else
+         #3.) always it have to end with else
+
+         #print ("A") if A>B else print("B")
+         # ! code to check the given char is vowel or consoment
+         #char = input ("enter the char:")
+         #if char =='a' or char  == 'e' or char =='i' or char 'o' or char == 'u':
+          #   print("is avowel")
+           #  else:
+              #   print("its consoment")
+
+    # ?or
+    #str1 ="aeiouaEIOU"
+    #IF  char in str1;
+    #print('vowel")
+         # else:
+            #  print("consoment")
+              # ! shorthand if else
+             # char = input ("enter the char;")
+              #str1 = "aeiouAEIOU"
+              #print("vowels") if char in str1 else print("consoment")
+
+""""""""
+
+# ! -----> elif ladder using short hand if else
+
+   # Eg:1
+
+    # ? find greatest among 3 v ariables using short hand if else
+    #a= 8
+    #b= 5
+    #c= 9
+
+    #print ('A is grater") if a>b and else print("B is grater")
+     #      if b>a and b>c else print("c is grater")
+
+"""""
+
+
+ # ! -------> looping
+#looping can be implimented using
+ #for loop
+ #while loop
+ #-----> for loop
+  #for loop is used for iteraton, if we know the number of lines the loop have to  run
+  #it is used to iteration
+
+  # ! for syntax in python
+  #for userdefind variable in range (start, end ,stop):deafult step value is 1
+  #statement
+  #statemen
+  #statement
+  # ? Eg:1
+
+
+  #to print the value from 1 to 10 using for  loop
+  #for 1 range (0,10): #(n, n-1)
+      #print(1)
+   #   print("hello")
+
+
+
+      # ? Eg:2
+    #  for val in range (1, 15, 2):
+     #     print(val)
+
+
+      #  for val in range (1,15 3)
+       # print(val)
+
+"""""
+
+
+       
+       # ? Eg:3
+
+# ! print the output of 7th multipication table from 21 t0 43
+for val in range (1,10+1,):
+ print('7','x', val, '=', val*7)
+ 
+ #meyhod 2
+ ans ="7 x {}={}"
+print(ans,format(val, val*7))
+
+# method 3
+      #print(f"7 x {val}={val*7}")
+
+
+#?Eg:5
+# brteak ---> used to terminate the loop
+for val  in range (1,10):
+    print(val)
+    if val ==6:
+        break
+
+    
+    # ? Eg: 6
+    # for all in range(1,10):
+    if val ==6:
+        print(val)
+        break
+
+
+    # ! continue
+
+    # Eg:1
+    
+   for val in range (1,):
+       if val ==6:
+         continue
+        print(val)
+    
+     #   ! practise problems
+      #  ? print the even number between 20 to 40
+      
+    for val in range (20,41):
+        print(val) %2==0:
+            print(val)
+
+            
+          
+              
+        
+        
+
+
+
+
+        
+              
+                
+                
+                 
+         
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+       
+                     
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
